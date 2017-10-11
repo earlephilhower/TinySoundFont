1fa85d41b04aaaeb6abd914293f292a007f18890
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-10-11 15:33:30 -0700

    IBAG to one-at-a-time

--- tsf.h
+++ tsf.h
@@ -297,7 +297,7 @@ struct tsf_hydra
 //	struct tsf_hydra_pmod *pmods;
 //	struct tsf_hydra_pgen *pgens;
 //	struct tsf_hydra_inst *insts;
-	struct tsf_hydra_ibag *ibags;
+//	struct tsf_hydra_ibag *ibags;
 	struct tsf_hydra_imod *imods;
 	struct tsf_hydra_igen *igens;
 	struct tsf_hydra_shdr *shdrs;
@@ -577,10 +577,16 @@ static void tsf_load_presets(tsf* res, struct tsf_hydra *hydra)
 				struct tsf_hydra_inst inst, instNext;
 				get_inst(hydra, pgen.genAmount.wordAmount, &inst);
 				get_inst(hydra, pgen.genAmount.wordAmount+1, &instNext);
-				for (pibag = hydra->ibags + inst.instBagNdx, pibagEnd = hydra->ibags + instNext.instBagNdx; pibag != pibagEnd; pibag++)
-					for (pigen = hydra->igens + pibag->instGenNdx, pigenEnd = hydra->igens + pibag[1].instGenNdx; pigen != pigenEnd; pigen++)
+				struct tsf_hydra_ibag ibag;
+				int ibagIdx, ibagEndIdx;
+				for (ibagIdx = inst.instBagNdx, get_ibag(hydra, ibagIdx, &ibag), ibagEndIdx = instNext.instBagNdx; ibagIdx != ibagEndIdx; ibagIdx++, get_ibag(hydra, ibagIdx, &ibag) )
+				{
+					struct tsf_hydra_ibag ibagNext;
+					get_ibag(hydra, ibagIdx + 1, &ibagNext);
+					for (pigen = hydra->igens + ibag.instGenNdx, pigenEnd = hydra->igens + ibagNext.instGenNdx; pigen != pigenEnd; pigen++)
 						if (pigen->genOper == GenSampleID)
 							preset->regionNum++;
+				}
 			}
 		}
 
@@ -621,12 +627,16 @@ static void tsf_load_presets(tsf* res, struct tsf_hydra *hydra)
 					struct tsf_hydra_inst inst, instNext;
 					get_inst(hydra, whichInst, &inst);
 					get_inst(hydra, whichInst + 1, &instNext);
-					for (pibag = hydra->ibags + inst.instBagNdx, pibagEnd = hydra->ibags + instNext.instBagNdx; pibag != pibagEnd; pibag++)
+					struct tsf_hydra_ibag ibag;
+					int ibagIdx, ibagEndIdx;
+					for (ibagIdx = inst.instBagNdx, get_ibag(hydra, ibagIdx, &ibag), ibagEndIdx = instNext.instBagNdx; ibagIdx != ibagEndIdx; ibagIdx++, get_ibag(hydra, ibagIdx, &ibag))
 					{
 						// Generators.
 						struct tsf_region zoneRegion = instRegion;
 						int hadSampleID = 0;
-						for (pigen = hydra->igens + pibag->instGenNdx, pigenEnd = hydra->igens + pibag[1].instGenNdx; pigen != pigenEnd; pigen++)
+						struct tsf_hydra_ibag ibagNext;
+						get_ibag(hydra, ibagIdx + 1, &ibagNext);
+						for (pigen = hydra->igens + ibag.instGenNdx, pigenEnd = hydra->igens + ibagNext.instGenNdx; pigen != pigenEnd; pigen++)
 						{
 							if (pigen->genOper == GenSampleID)
 							{
@@ -704,7 +714,7 @@ static void tsf_load_presets(tsf* res, struct tsf_hydra *hydra)
 						}
 
 						// Handle instrument's global zone.
-						if (pibag == hydra->ibags + inst.instBagNdx && !hadSampleID)
+						if (ibagIdx == inst.instBagNdx && !hadSampleID)
 							instRegion = zoneRegion;
 
 						// Modulators (TODO)
@@ -1174,7 +1184,7 @@ TSFDEF tsf* tsf_load(struct tsf_stream* stream)
 				else if GetChunkOffset(pmod)
 				else if GetChunkOffset(pgen)
 				else if GetChunkOffset(inst)
-				else if HandleChunk(ibag)
+				else if GetChunkOffset(ibag)
 				else if HandleChunk(imod)
 				else if HandleChunk(igen)
 				else if HandleChunk(shdr)
@@ -1206,7 +1216,7 @@ TSFDEF tsf* tsf_load(struct tsf_stream* stream)
 		}
 		else stream->skip(stream->data, chunkList.size);
 	}
-	if (!hydra.phdrNum || !hydra.pbagNum || !hydra.pmodNum || !hydra.pgenNum || !hydra.instNum || !hydra.ibags || !hydra.imods || !hydra.igens || !hydra.shdrs)
+	if (!hydra.phdrNum || !hydra.pbagNum || !hydra.pmodNum || !hydra.pgenNum || !hydra.instNum || !hydra.ibagNum || !hydra.imods || !hydra.igens || !hydra.shdrs)
 	{
 		//if (e) *e = TSF_INVALID_INCOMPLETE;
 	}
@@ -1231,7 +1241,7 @@ TSFDEF tsf* tsf_load(struct tsf_stream* stream)
 	//TSF_FREE(hydra.pmods);
 	//TSF_FREE(hydra.pgens);
 	//TSF_FREE(hydra.insts);
-	TSF_FREE(hydra.ibags);
+	//TSF_FREE(hydra.ibags);
 	TSF_FREE(hydra.imods); TSF_FREE(hydra.igens); TSF_FREE(hydra.shdrs);
 	return res;
 }
