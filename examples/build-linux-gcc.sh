9f7a2f0bdb679f892a7132d4fdb8092b88d98252
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-10-12 08:35:11 -0700

    Convert miditones to use TSF streams

--- examples/build-linux-gcc.sh
+++ examples/build-linux-gcc.sh
@@ -5,5 +5,7 @@ gcc -g -Wall example1.c minisdl_audio.c -lm -ldl -lpthread -o example1-linux-`un
 rm -f example2-linux-`uname -m`
 gcc -g -Wall example2.c minisdl_audio.c -lm -ldl -lpthread -o example2-linux-`uname -m`
 rm -f miditsf
-gcc -g -Wall miditsf.c -lm -ldl -lpthread -o miditsf
+gcc -g -Wall miditsf.c -lm -o miditsf
+rm -f miditones
+gcc -g -Wall miditones.c -lm -o miditones
 #echo Done!
